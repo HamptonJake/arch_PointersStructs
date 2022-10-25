fb76d1abc39267543ef9260e6ddef9fe0df763d8
HamptonJake <49708455+HamptonJake@users.noreply.github.com>
2022-10-25 03:14:02 -0600

    Can read text file

--- Lab1.c
+++ Lab1.c
@@ -6,18 +6,33 @@
 int main(int argc, char **argv){
 	/* One way to initialize a POI, by using a pointer */
 	struct poi_t_struct *w = (struct poi_t_struct*) malloc(sizeof(struct poi_t_struct));
-	
+
 	/* Another way to initialize a POI, by using a struct, then getting the address of the struct and storing it as a pointer */
 	struct poi_t_struct w2 = {"William Basquez", 123452, 3, NULL};
 	struct poi_t_struct *ptr_w2 = &w2;
-	
+
 	w->name = "William Basquez";
 	w->id = 12345;
 	w->threat_level = 3;
 	w->next = NULL;
-	
+
 	print_record(w);
 	print_record(ptr_w2);
 
-    return 0;
+    char file_path[260];
+	printf("Please enter the path of the file you want to read: \n");
+	scanf("%s", file_path);
+
+	//printf("Is :%s correct?\n", file_path);
+
+	FILE * file_to_open = fopen(file_path, "r");
+
+    char line[100];
+
+    while(fgets(line, sizeof(line), file_to_open)){
+        //print the line
+        printf("%s", line);
+    }
+
+	return 0;
 }
